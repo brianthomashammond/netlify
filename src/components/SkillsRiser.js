0b6abcadb0249fddaa1f461898251cef0d8eafa4
brianthomashammond <brianthomashammond@gmail.com>
2021-04-13 14:37:19 -0700

    converts SkillsRiser to functional component

--- src/components/SkillsRiser.js
+++ src/components/SkillsRiser.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class SkillsRiser extends Component {
-
-  render() {
+const SkillsRiser = (props) => {
     return (
-      <div className={this.props.skillsPosition}>
+        <div className={props.skillsPosition}>
         <div className="skills div">
-          <div className="skills div">
+            <div className="skills div">
             <i className="skills javascript-image-wrapper"></i>
             <i className="skills css3-image-wrapper"></i>
             <i className="skills html5-image-wrapper"></i>
             <i className="skills ruby-image-wrapper"></i>
             <i className="skills detox-image-wrapper"></i>
-          </div>
-          <div className="skills div">
+            </div>
+            <div className="skills div">
             <i className="skills react-image-wrapper"></i>
             <i className="skills rails-image-wrapper"></i>
             <i className="skills postgresql-image-wrapper"></i>
             <i className="skills postman-image-wrapper"></i>
             <i className="skills git-image-wrapper"></i>
-          </div>
+            </div>
         </div>
-      </div>
+        </div>
     )
-  }
 }
+
+export default SkillsRiser
