cb85e5f38ffd47c1309d44b51753a9fa22243a5d
brianthomashammond <brianthomashammond@gmail.com>
2021-04-16 17:28:45 -0700

    converts navbar to functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,58 +1,56 @@
-import React, { Component } from "react";
+import React from "react"
 
-export default class Navbar extends Component {
-  render() {
+const Navbar = (props) => {
     return (
-      <div className={this.props.navPosition}>
-        <div className="navbar">
-          <div className="typewriter">
-            >> Brian Thomas Hammond - Web Developer
-          </div>
-          <span className="navbar projects">
-            <a
-              href="#projects"
-              className="navbar projects link"
-              onClick={() => {
-                this.props.projectChange();
-              }}
-            >
-              Projects
-            </a>
-          </span>
-          <span className="navbar skills">
-            <a
-              href="#skills"
-              className="navbar skills link"
-              onClick={() => {
-                this.props.skillsChange();
-              }}
-            >
-              {" "}
-              Skills{" "}
-            </a>
-          </span>
-          <span className="navbar resume">
-            <a
-              href="#resume"
-              className="navbar resume link"
-              onClick={() => {
-                window.open(this.props.file);
-              }}
-            >
-              {" "}
-              Resume{" "}
-            </a>
-          </span>
-          <span className="navbar contact">
-            <a
-              href="mailto:brianthomashammond@gmail.com"
-              className="navbar contact link"
-            >
-              Contact
-            </a>
-          </span>
+        <div className={props.navPosition}>
+            <div className="navbar">
+            <div className="typewriter">
+                >> Brian Thomas Hammond - Web Developer
+            </div>
+            <span className="navbar projects">
+                <a
+                href="#projects"
+                className="navbar projects link"
+                onClick={() => {
+                    props.projectChange()
+                }}
+                >
+                Projects
+                </a>
+            </span>
+            <span className="navbar skills">
+                <a
+                href="#skills"
+                className="navbar skills link"
+                onClick={() => {
+                    props.skillsChange()
+                }}
+                >
+                {" "}Skills{" "}
+                </a>
+            </span>
+            <span className="navbar resume">
+                <a
+                href="#resume"
+                className="navbar resume link"
+                onClick={() => {
+                    window.open(props.file)
+                }}
+                >
+                {" "}Resume{" "}
+                </a>
+            </span>
+            <span className="navbar contact">
+                <a
+                href="mailto:brianthomashammond@gmail.com"
+                className="navbar contact link"
+                >
+                Contact
+                </a>
+            </span>
+            </div>
         </div>
-      </div>
-    );
-  }
+    )
 }
+
+export default Navbar
