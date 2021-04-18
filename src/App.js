ba3bdaade6601b2f7aa343bda2651eb9d33a695c
brianthomashammond <brianthomashammond@gmail.com>
2021-04-17 17:22:11 -0700

    conerts app to functional component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,52 @@
-import React, { Component } from "react";
-import "./App.css";
-import Navbar from "./components/Navbar.js";
-import ProjectDrop from "./components/ProjectDrop.js";
-import SkillsRiser from "./components/SkillsRiser.js";
-import file from "./assets/20201025 Brian Thomas Hammond Resume.pdf";
-// import { motion } from "framer-motion"
+import React, { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import ProjectDrop from './components/ProjectDrop'
+import SkillsRiser from './components/SkillsRiser'
+import file from './assets/20201025 Brian Thomas Hammond Resume.pdf'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      projectsPosition: "projects off",
-      navPosition: "navbar center",
-      skillsPosition: "skills off",
-      file: file,
-    };
-  }
+const App = () => {
+    const [ projectsPosition, setProjectsPosition ] = useState('projects off')
+    const [ navPosition, setNavPosition ] = useState('navbar center')
+    const [ skillsPosition, setSkillsPosition ] = useState('skills off')
 
-  projectChange = () => {
-    if (this.state.skillsPosition === "skills off") {
-      this.setState({
-        projectsPosition: "projects fadeIn",
-        navPosition: "navbar bottom",
-      });
-    } else {
-      this.setState({
-        projectsPosition: "projects fadeIn",
-        navPosition: "navbar bottom",
-        skillsPosition: "skills fadeOut",
-      });
+
+    const projectChange = () => {
+        if (skillsPosition === 'skills off') {
+            setProjectsPosition('projects fadeIn')
+            setNavPosition('navbar bottom')
+        } else {
+            setProjectsPosition('projects fadeIn')
+            setNavPosition('navbar bottom')
+            setSkillsPosition('skills fadeOut')
+        }
     }
-  };
 
-  skillsChange = () => {
-    if (this.state.projectsPosition === "projects off") {
-      this.setState({
-        navPosition: "navbar top",
-        skillsPosition: "skills fadeIn",
-      });
-    } else {
-      this.setState({
-        projectsPosition: "projects fadeOut",
-        navPosition: "navbar top",
-        skillsPosition: "skills fadeIn",
-      });
+    const skillsChange = () => {
+        if (projectsPosition === 'projects off') {
+            setNavPosition('navbar top')
+            setSkillsPosition('skills fadeIn')
+        } else {
+            setProjectsPosition('projects fadeOut')
+            setNavPosition('navbar top')
+            setSkillsPosition('skills fadeIn')
+        }
     }
-  };
 
-  render() {
     return (
-      <>
-        <div>
-          <ProjectDrop projectsPosition={this.state.projectsPosition} />
-          <Navbar
-            navPosition={this.state.navPosition}
-            projectChange={this.projectChange}
-            skillsChange={this.skillsChange}
-            file={this.state.file}
-          />
-          <SkillsRiser skillsPosition={this.state.skillsPosition} />
-        </div>
-      </>
-    );
-  }
+        <>
+            <div>
+                <ProjectDrop projectsPosition={projectsPosition} />
+                <Navbar
+                    navPosition={navPosition}
+                    projectChange={projectChange}
+                    skillsChange={skillsChange}
+                    file={file}
+                />
+                <SkillsRiser skillsPosition={skillsPosition} />
+            </div>
+        </>
+    )
 }
+
+export default App
